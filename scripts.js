71d2ecbe54c5f6ab56d74f2c69e4896e65a0761c
Mateusz <matt.borowczyk@gmail.com>
2020-09-27 14:53:41 +0200

    html&css setup and skeleton of js functionalities

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
+const Player = (name, choice) => {
+    const getName = () => name;
+    const score = 0;
+    const getChoice = () => choice;
 
+    return {
+        getName,
+        getChoice
+    }
+};
+
+const Gameboard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
+    const getBoard = () => board;
+    const resetBoard = () => {
+        board = ["", "", "", "", "", "", "", "", ""];
+        for(let i = 0; i < board.length; i++){
+            document.getElementById(i+1).innerHTML = "";
+        }
+    }
+    const makeMove = (player) => {
+
+    }
+    return {
+        getBoard,
+        resetBoard,
+        makeMove
+    };
+})();
+
+const GameController = (() => {
+    const render = (id, choice) => {
+        document.getElementById(id).innerHTML= `<span>${choice}</span>`
+    }
+})();
